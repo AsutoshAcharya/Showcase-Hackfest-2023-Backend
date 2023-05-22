079312e3fecc44416ac4ad1cb28550303e664aa5
Asutosh Acharya <asutosh246@gmail.com>
2023-05-22 19:36:12 +0530

    api changes

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,33 +1,31 @@
 import { Configuration, OpenAIApi } from "openai";
 import dotenv from "dotenv";
-dotenv.config();
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+import path from "path";
 
-const openai = new OpenAIApi(configuration);
+// const openai = new OpenAIApi(configuration);
 
-export const aiChat = async (req, res, next) => {
-  try {
-    const prompt = req.body.prompt;
+// export const aiChat = async (req, res, next) => {
+//   try {
+//     const prompt = req.body.prompt;
+//     console.log(prompt);
 
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `${prompt}`,
-      temperature: 0.7,
-      max_tokens: 3000,
-      top_p: 1,
-      frequency_penalty: 0.5,
-      presence_penalty: 0,
-    });
-
-    res.status(200).send({
-      bot: response.data.choices[0].text,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+//     const response = await openai.createCompletion({
+//       model: "text-davinci-003",
+//       prompt: `${prompt}`,
+//       temperature: 0.7,
+//       max_tokens: 3000,
+//       top_p: 1,
+//       frequency_penalty: 0.5,
+//       presence_penalty: 0,
+//     });
+//     console.log(response);
+//     res.status(200).send({
+//       bot: response.data.choices[0].text,
+//     });
+//   } catch (err) {
+//     next(err);
+//   }
+// };
 
 export const getChat = async (req, res, next) => {
   try {
--- index.js
+++ index.js
@@ -4,11 +4,41 @@ import mongoose from "mongoose";
 import cors from "cors";
 import authRoute from "./routes/auth.js";
 import chatRoute from "./routes/aichat.js";
+import { Configuration, OpenAIApi } from "openai";
 
 mongoose.set("strictQuery", true);
 
 dotenv.config();
 const app = express();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+export const aiChat = async (req, res, next) => {
+  try {
+    const prompt = req.body.prompt;
+    console.log(prompt);
+
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 0.7,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    });
+    console.log(response);
+    res.status(200).send({
+      bot: response.data.choices[0].text,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
 
 const connect = async () => {
   try {
@@ -28,7 +58,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/auth", authRoute);
-app.use("/chat", chatRoute);
+app.use("/chat", aiChat);
 
 app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
